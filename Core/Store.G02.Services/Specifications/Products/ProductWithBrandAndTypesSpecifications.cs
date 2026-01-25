57098d6adc0e7691eab7bdc8fa88d082678bb9ec
Hassan Taha <hassanrajebt@gmail.com>
2026-01-25 23:09:02 +0200

    Product Module - Add Search to Get All Products

--- Core/Store.G02.Services/Specifications/Products/ProductWithBrandAndTypesSpecifications.cs
+++ Core/Store.G02.Services/Specifications/Products/ProductWithBrandAndTypesSpecifications.cs
@@ -18,12 +18,14 @@ namespace Store.G02.Services.Specifications.Products
 
 
         // null & null
-        public ProductsWithBrandAndTypeSpecifications(int? brandId, int? typeId, string? sort) : base
+        public ProductsWithBrandAndTypeSpecifications(int? brandId, int? typeId, string? sort, string? search) : base
             (
             P =>
              (!brandId.HasValue || P.BrandId == brandId) 
             &&
              (!typeId.HasValue || P.TypeId == typeId)
+            &&
+             (string.IsNullOrEmpty(search) || P.Name.ToLower().Contains(search.ToLower()))
             )
         {
             ApplySort(sort);
